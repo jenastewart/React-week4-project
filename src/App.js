0d16b30cee16e1704f37dedb2b46d10786075a23
Jena Stewart <66694416+jenastewart@users.noreply.github.com>
2024-11-09 13:21:13 -0800

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Search from "./Search";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather Search</h1>
       <Search />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <p>
+      This project was coded by Jena Stewart. It is open-sourced on{" "}
+      <a
+        href="https://github.com/jenastewart/React-week4-project"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Github
+      </a>{" "}
+      and hosted on{" "}
+      <a
+        href="https://jenas-react-weather-app.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Netlify
+      </a>
+    </p>
+  );
+}
